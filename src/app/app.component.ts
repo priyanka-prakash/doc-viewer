6cb8a5b8a51efc6f8c7989cf729490ff106b4775
priyanka <priyanka.prakash@fingent.com>
2020-04-24 19:03:50 +0530

    doc-viewer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'document-viewer';
+  doc_url:string;
+  file:any;
+
+  ngOnInit(): void {
+    // this.doc_url = "https://file-examples.com/wp-content/uploads/2017/02/file-sample_100kB.docx";
+    this.doc_url = "http://www.africau.edu/images/default/sample.pdf";
+  }
+
+ 
+  
 }
